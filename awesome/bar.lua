3ddc3bae33cb3abc8e2206fdb1dcd68a9a24a6f8
LegoRaft <50689727+LegoRaft@users.noreply.github.com>
2022-09-28 17:01:31 +0200

    updated bar to be minimal

--- awesome/bar.lua
+++ awesome/bar.lua
@@ -16,32 +16,35 @@ require("awful.hotkeys_popup.keys")
 
 awful.screen.connect_for_each_screen(function(s)
 
-    -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag({ "1"}, s, awful.layout.layouts[1])
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
+    mytextclock = wibox.widget.textclock()
+
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
     s.mylayoutbox = awful.widget.layoutbox(s)
+
     s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
-    }
+        awful.button({ }, 1, function () awful.layout.inc( 1) end)
+    ))
 
     -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist {
         screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
+        filter  = awful.widget.tasklist.filter.focused,
+        buttons = tasklist_buttons,
+        widget_template = {
+            {
+                id = "text_role",
+                widget = wibox.widget.textbox,
+            },
+            left = 10,
+            widget = wibox.container.margin
+        }
     }
+
+    beautiful.tasklist_plain_task_name = true
+    
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s })
@@ -51,16 +54,12 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
-            wibox.widget.systray(),
             mytextclock,
+            wibox.widget.systray(),
             s.mylayoutbox,
         },
     }
