23899210d79a42e5aaf171ac35b21467ebf15d6b
LegoRaft <50689727+LegoRaft@users.noreply.github.com>
2022-09-28 17:23:56 +0200

    updated border width

--- awesome/rules.lua
+++ awesome/rules.lua
@@ -13,18 +13,21 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
 
 awful.rules.rules = {
-    -- All clients will match this rule.
+-- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
+      properties = { 
+        border_width = 3,
+        border_color = beautiful.border_normal,
+        focus = awful.client.focus.filter,
+        raise = true,
+        keys = clientkeys,
+        buttons = clientbuttons,
+        screen = awful.screen.preferred,
+        placement = awful.placement.no_overlap+awful.placement.no_offscreen
      }
     },
 
