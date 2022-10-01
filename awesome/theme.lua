cf9ba2b6c0e2e5af4eead555f331643004d33319
legoraft <50689727+legoraft@users.noreply.github.com>
2022-10-01 13:03:28 +0200

    updated theme with new icons

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -3,7 +3,7 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
-local themes_path = gfs.get_themes_dir()
+local themes_path = gfs.get_configuration_dir()
 
 local theme = {}
 
@@ -26,24 +26,11 @@ theme.border_normal = "#161a1a"
 theme.border_focus  = "#60bfbf"
 theme.border_marked = "#91231c"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.round = 10
 
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_floating = themes_path.."icons/floating-icon.svg"
+theme.layout_spiral = themes_path.."icons/spiral-icon.svg"
+theme.layout_tile = themes_path.."icons/tile-icon.svg"
 
 theme.icon_theme = nil
 
