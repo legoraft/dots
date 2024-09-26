b43ce56bb30257c3824089992af8e9d9c4095cca
legoraft <legoraft@legoraft.com>
2024-09-26 08:21:33 +0200

    Added waybar config

---
+++ .config/waybar/xbps-updates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pkgs=$(xbps-install -nuM | awk '{print $1}')
+pkg_count=$(echo $pkgs | wc -w)
+pkg_list=$(echo $pkgs | sed 's/ /\\r/g')
+
+if [ $pkg_count -gt 0 ]; then
+  notify-send "Updates available" "There are $pkg_count updates available for your system"
+fi
+
+echo "{\"text\":\"$pkg_count\", \"tooltip\":\"$pkg_list\"}"
