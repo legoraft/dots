ffab4a19568fdc2a0742e4527c7e8d8c0be27a75
legoraft <git@legoraft.com>
2024-11-05 09:05:03 +0100

    feat: added help screen and options to updates

--- .config/waybar/xbps-updates.sh
+++ .config/waybar/xbps-updates.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+Help() {
+  echo "This script allows you to check for updates and update the system from waybar."
+  echo
+  echo "Syntax: xbps-updates [-c|u|h]"
+  echo "options:"
+  echo "c     Checks for updates"
+  echo "u     Updates the system"
+  echo "h     Display this help message."
+  echo
+}
+
 Get_updates() {
   pkgs=$(xbps-install -nuM | awk '{print $1}')
   pkg_count=$(echo $pkgs | wc -w)
@@ -11,3 +22,20 @@ Get_updates() {
 
   echo "{\"text\":\"$pkg_count\", \"tooltip\":\"$pkg_list\"}"
 }
+
+while getopts ":hcu" option; do
+  case $option in
+    h) # displays help
+      Help
+      exit;;
+    c) # checks for updates
+      Get_updates
+      exit;;
+    u) # updates system
+      echo "update"
+      exit;;
+    \?) # catchall
+      Help
+      exit;;
+  esac
+done
