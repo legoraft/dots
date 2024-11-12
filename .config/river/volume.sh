0420f0553be5e6d9677ab3b54c3dba7d01f61224
legoraft <git@legoraft.com>
2024-11-12 11:50:01 +0100

    feat: added brightness and volume logic

--- .config/river/volume.sh
+++ .config/river/volume.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+type="$1"
+action="$2"
+icon=""
+percent=0
+
 Help() {
     echo "A volume script to display an overlay while changing the volume."
     echo
@@ -12,13 +17,11 @@ Help() {
 }
 
 Overlay() {
-    percent=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
-    percent=${percent:2}
-
-    eww update icon="󰕾" percent=$percent
-    eww open overlay
+    eww update icon=$icon percent="$percent" && eww open overlay
     
-    if [ $counter -eq 0 ]; then
+    sleep 1
+    
+    if [ $bar_count -lt 1 ]; then
         eww close overlay
     fi
 }
@@ -28,21 +31,29 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
-while getopts ":hdu" option; do
-    case $option in
-        h) # displays help
-            Help
-            exit;;
-        d) # screenshot full screen
-            wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-            Overlay
-            exit;;
-        u) # select a portion and screenshot
+if [ $type == "volume" ]; then
+    percent=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+    percent=${percent//./}
+    icon="󰕾"
+    
+    if [ $action == "up" ]; then
+        if [ $percent -lt 100 ]; then
             wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-            Overlay
-            exit;;
-        \?) # catchall
-            Help
-            exit;;
-    esac
-done
+        else
+            exit
+        fi
+    elif [ $action == "down" ]; then
+        wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    fi
+elif [ $type == "brightness" ]; then
+    percent=$(brightnessctl i | grep -E -o '[0-9]+' | sed -n 2p)
+    icon="󰖨"
+    
+    if [ $action == "up" ]; then
+        brightnessctl set +5%
+    elif [ $action == "down" ]; then
+        brightnessctl set 5%-
+    fi
+fi
+
+Overlay
