9ec00887c93a4f5a58214acfd65af0bce48b2cb1
legoraft <git@legoraft.com>
2024-11-19 13:27:04 +0100

    fix: package count is now updated in Get_updates()

--- .config/waybar/xbps-updates.sh
+++ .config/waybar/xbps-updates.sh
@@ -11,11 +11,11 @@ Help() {
   echo
 }
 
-pkgs=$(xbps-install -nuM | awk '{print $1}')
-pkg_count=$(echo $pkgs | wc -w)
-pkg_list=$(echo $pkgs | sed 's/ /\\r/g')
-
 Get_updates() {
+  pkgs=$(xbps-install -nuM | awk '{print $1}')
+  pkg_count=$(echo $pkgs | wc -w)
+  pkg_list=$(echo $pkgs | sed 's/ /\\r/g')
+
   if [ $pkg_count -gt 0 ]; then
     notify-send "Updates available" "There are $pkg_count updates available for your system."
   fi
