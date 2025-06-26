3a9e3f6c9cc090d9a0f3296bacf81c6a13fbc6cc
legoraft <git@legoraft.com>
2025-06-26 11:36:14 +0200

    feat: added small delay between closing windows and shutting down

--- waybar/.config/waybar/scripts/killwindows.sh
+++ waybar/.config/waybar/scripts/killwindows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while hyprctl clients -j | jq -r '.[].class' | grep -qE '.*'; do
-    hyprctl dispatch killwindow "class:^(kitty)$"
+    hyprctl dispatch killwindow "class:^(kitty|alacritty)$"
     hyprctl dispatch closewindow "class:.*"
 done
